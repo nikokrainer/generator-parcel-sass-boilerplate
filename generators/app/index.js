087a94accaf177adfe4d2b6d72f1c98911801863
Niko <nikokrainer02@gmail.com>
2021-10-11 21:57:05 +0200

    Added .htmlnanorc & .sassrc file

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,29 +1,34 @@
-const Generator = require('yeoman-generator');
+const Generator = require("yeoman-generator")
 module.exports = class extends Generator {
-	welcome() {
-		this.log('Welcome to the Parcel and Sass Generator!');
-	}
+  welcome() {
+    this.log("Welcome to the Parcel and Sass Generator!")
+  }
 
-	writing() {
-		this.fs.copy(
-			this.templatePath('_gitignore'),
-			this.destinationPath('.gitignore')
-		);
-		this.fs.copy(
-			this.templatePath('package.json'),
-			this.destinationPath('package.json')
-		);
-		this.fs.copy(
-			this.templatePath('package-lock.json'),
-			this.destinationPath('package-lock.json')
-		);
-		this.fs.copy(this.templatePath('src'), this.destinationPath('src'));
-	}
+  writing() {
+    this.fs.copy(
+      this.templatePath("_gitignore"),
+      this.destinationPath(".gitignore")
+    )
+    this.fs.copy(
+      this.templatePath("package.json"),
+      this.destinationPath("package.json")
+    )
+    this.fs.copy(
+      this.templatePath("package-lock.json"),
+      this.destinationPath("package-lock.json")
+    )
+	this.fs.copy(this.templatePath(".htmlnanorc"), 
+	this.destinationPath(".htmlnanorc"))
+	this.fs.copy(this.templatePath(".sassrc"), 
+	this.destinationPath(".sassrc"))
 
-	end() {
-		this.log(' ');
-		this.log(
-			'Finished generating! Before running, make sure you have the Parcel installed globally.'
-		);
-	}
-};
+    this.fs.copy(this.templatePath("src"), this.destinationPath("src"))
+  }
+
+  end() {
+    this.log(" ")
+    this.log(
+      "Finished generating! Before running, make sure you have the Parcel installed globally."
+    )
+  }
+}
